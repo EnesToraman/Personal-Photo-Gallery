a96847348baed874ed76757aabda04bf2467e11e
EnesToraman <enstrmn@hotmail.com>
2021-10-31 16:45:18 +0300

    Firebase configurations are done.

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app'
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtX0eGBXMoTmdQ7MNuFSNfSmCFsng0C48",
+    authDomain: "personal-photo-gallery-32b21.firebaseapp.com",
+    projectId: "personal-photo-gallery-32b21",
+    storageBucket: "personal-photo-gallery-32b21.appspot.com",
+    messagingSenderId: "275548052276",
+    appId: "1:275548052276:web:c2d61ef997b6631d8da361"
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { storage, firestore, auth, timestamp };
